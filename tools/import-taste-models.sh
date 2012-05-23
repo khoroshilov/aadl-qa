khoroshilov/aadl-qa

commit 225de9c725c399a97bdd0f1b3512c608722e7294

REQ is an optional property that means an identifier of requirement from spec that tested by the test case

tools/import-taste-models.sh
@@ -30,7 +30,6 @@ for v in `ls ${TASTEGUIDIR}/examples/ | grep -v svn`; do
          (cd /tmp/ && cp -f dataview.aadl ${EXPORTDIR}) >/dev/null 2>&1
          (cd /tmp/ && cp -f deploymentview.aadl ${EXPORTDIR}) >/dev/null 2>&1
          (cd /tmp/ && cp -f interfaceview.aadl ${EXPORTDIR}) >/dev/null 2>&1
-         echo REQ=/Requirements/01-Syntax/identifier/01-l > ${EXPORTDIR}/MANIFEST.TC
          echo EXPECTED_RESULT=VALID >> ${EXPORTDIR}/MANIFEST.TC
          echo OCARINA_USE_COMPONENTS_LIBRARY=NO >> ${EXPORTDIR}/MANIFEST.TC
          echo OCARINA_INSTANTIATE=YES >> ${EXPORTDIR}/MANIFEST.TC

commit 4caa948cdc31e274992b9a328d0fffad949a7811

Fix import script to rewrite MANIFEST.TC file (bug introduced in 225de9c)

tools/import-taste-models.sh
@@ -30,7 +30,7 @@ for v in `ls ${TASTEGUIDIR}/examples/ | grep -v svn`; do
          (cd /tmp/ && cp -f dataview.aadl ${EXPORTDIR}) >/dev/null 2>&1
          (cd /tmp/ && cp -f deploymentview.aadl ${EXPORTDIR}) >/dev/null 2>&1
          (cd /tmp/ && cp -f interfaceview.aadl ${EXPORTDIR}) >/dev/null 2>&1
-         echo EXPECTED_RESULT=VALID >> ${EXPORTDIR}/MANIFEST.TC
+         echo EXPECTED_RESULT=VALID > ${EXPORTDIR}/MANIFEST.TC
          echo OCARINA_USE_COMPONENTS_LIBRARY=NO >> ${EXPORTDIR}/MANIFEST.TC
          echo OCARINA_INSTANTIATE=YES >> ${EXPORTDIR}/MANIFEST.TC
          echo OCARINA_INSTANCES_NAMES=deploymentview.others interfaceview.others >> ${EXPORTDIR}/MANIFEST.TC